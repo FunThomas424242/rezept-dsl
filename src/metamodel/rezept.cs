d16164652fd14e5f475c635d099c0be67bac70ea
Thomas Schubert <funthomas424242@googlemail.com>
2012-03-04 17:44:34 +0100

    changed to utf-8 encoding

--- src/metamodel/rezept.cs
+++ src/metamodel/rezept.cs
@@ -41,19 +41,19 @@ TOKENSTYLES {
 	"Untertitel:" COLOR #7F0055, BOLD;
 	"Kategorie:" COLOR #7F0055, BOLD;
 	"Tag:" COLOR #7F0055, BOLD;
-	"Diät:" COLOR #7F0055, BOLD;
+	"DiÃ¤t:" COLOR #7F0055, BOLD;
 	"Stoff:" COLOR #7F0055, BOLD;
 	"ProduktRef:" COLOR #7F0055, BOLD;
 	"Produkt" COLOR #7F0055, BOLD;
 	"RezeptSammlung" COLOR #7F0055, BOLD;
 	"groupId","artifactId","version" COLOR #7F0055, BOLD;
-	"Letzte Änderung:" COLOR #7F0055, BOLD;
+	"Letzte Ã„nderung:" COLOR #7F0055, BOLD;
 	"Modul" COLOR #7F0055, BOLD;
 	"Verpackung:" COLOR #7F0055, BOLD;
 	"EAN:" COLOR #7F0055, BOLD;
 	"UBA:" COLOR #7F0055, BOLD;
 	"Preis:" COLOR #7F0055, BOLD;
-	"Händler:" COLOR #7F0055, BOLD;
+	"HÃ¤ndler:" COLOR #7F0055, BOLD;
 	"Hersteller:" COLOR #7F0055, BOLD;
 	"Quelle:" COLOR #7F0055, BOLD;
 	"Aktion:" COLOR #7F0055, BOLD;
@@ -77,23 +77,23 @@ RULES {
 				menge
 				("EAN:" ean['"','"'] ".")? ("UBA:" uba['"','"'] ".")? 
 				preis? 
-				("Händler:" handler['"','"'] ".")?
-				("Hersteller:" hersteller['"','"'] ".")? "Letzte Änderung:" datumLetzteAenderung[DATUM] "." "}" ;
+				("HÃ¤ndler:" handler['"','"'] ".")?
+				("Hersteller:" hersteller['"','"'] ".")? "Letzte Ã„nderung:" datumLetzteAenderung[DATUM] "." "}" ;
 	Preis ::= "Preis:" betrag['"','"'] waehrung['"','"'] ".";
 	Rezept ::= "Rezept" id['"','"'] "{" 
 				"Titel:" titel['"','"'] "." ("Untertitel:" untertitel['"','"'] ".")? "Kategorie:" kategorie['"','"'] "."
-				"Letzte Änderung:" letzteAenderung[DATUM] "." tags+
+				"Letzte Ã„nderung:" letzteAenderung[DATUM] "." tags+
 				produkte* zutaten+ schritte+
 				quelle? bewertung? tipps*    
 			    "}";
 	StoffTag ::=  "Stoff:" stoff['"','"'] "." ; 
 	BenutzerTag ::=  "Tag:" bezeichnung['"','"'] ".";
-	DiaetTag ::= "Diät:" diaet['"','"'] "." ;
+	DiaetTag ::= "DiÃ¤t:" diaet['"','"'] "." ;
 	ProduktRef ::= "ProduktRef:" produkt['"','"'] "Menge:" menge['"','"'] "mal.";
 	Zutat ::= "Zutat:" name['"','"'] menge    ;
 	Menge ::= "Menge:" betrag['"','"'] einheit['"','"'] ".";
 	Arbeitsschritt ::= "Aktion:" beschreibung['"','"'];
 	Tipp ::= "Tipp:"  text['"','"'] ;
 	Rank ::= "Rank:" bewertung['"','"'] ;
-	Quelle ::= "Quelle:" "übernommen" ":" modifikationsArt['"','"'] "aus" beschreibung['"','"'] "." ;
+	Quelle ::= "Quelle:" "Ã¼bernommen" ":" modifikationsArt['"','"'] "aus" beschreibung['"','"'] "." ;
 }
