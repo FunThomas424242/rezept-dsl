58de4133a2f49c37806c789b7f291e79efbac0f7
Thomas Schubert <funthomas424242@googlemail.com>
2013-01-16 21:34:34 +0100

    Oganisation des Autors ist optional

--- src/metamodel/rezept.cs
+++ src/metamodel/rezept.cs
@@ -75,7 +75,7 @@ RULES {
 	BuchBeschreibung ::= "Buch" "{" imports* "Titel:" titel['"','"'] ";" autoren+ 
 	            ("Vorwort:" vorwort['"','"'])?  lizenz? ("Veröffentlicht am:" datumPublished[DATUM] ".")?  "}";
 	Autor ::= "Autor:" "{" "Vorname:" vorname['"','"'] "Nachname:" nachname['"','"'] 
-			   "Organisation:" organisationsName['"','"']? "ModifikationsNotiz:" modifikationsNotiz['"','"']  "}";
+			   ("Organisation:" organisationsName['"','"'])? "ModifikationsNotiz:" modifikationsNotiz['"','"']  "}";
 	Lizenz ::= "Lizenshinweis:" hinweis['"','"'] ("Lizenstext:" text[])? ;
 	RezeptImport ::=  "import rezepte"  importedResource['"','"'] ";" ;
 	Rezeptliste ::= "Rezeptliste" name['"','"']   ";" imports* rezepte+ ;	
