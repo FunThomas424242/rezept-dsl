34fb46d35a5e307c14d5c57c32290fc6055761cb
FunThomas424242 <funthomas424242@gmail.com>
2016-02-20 23:19:07 +0100

    Project Explorer aufgenommen
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- Plugins/rezeptDSL.product/src/rezeptDSL/product/parts/RezeptEditorPart.java
+++ Plugins/rezeptDSL.product/src/rezeptDSL/product/parts/RezeptEditorPart.java
@@ -13,45 +13,23 @@ package rezeptDSL.product.parts;
 import javax.annotation.PostConstruct;
 
 import org.eclipse.e4.ui.di.Focus;
-import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
 
-import com.github.funthomas424242.rezeptsammler.rezept.resource.rezept.IRezeptMetaInformation;
-import com.github.funthomas424242.rezeptsammler.rezept.resource.rezept.IRezeptTextResourcePluginPart;
+import com.github.funthomas424242.rezeptsammler.rezept.resource.rezept.ui.RezeptEditor;
 
-public class RezeptEditorPart implements IRezeptTextResourcePluginPart{
+public class RezeptEditorPart {
 
-	private Label label;
-	private TableViewer tableViewer;
+	private RezeptEditor editor = new RezeptEditor();
 
 	@PostConstruct
 	public void createComposite(Composite parent) {
-		parent.setLayout(new GridLayout());
-
-		label = new Label(parent, SWT.NONE);
-		label.setText("Sample table");
-
-		tableViewer = new TableViewer(parent);
-		tableViewer.add("Sample item 1");
-		tableViewer.add("Sample item 2");
-		tableViewer.add("Sample item 3");
-		tableViewer.add("Sample item 4");
-		tableViewer.add("Sample item 5");
-		tableViewer.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
+		editor.createPartControl(parent);
 	}
 
 	@Focus
 	public void setFocus() {
-		tableViewer.getTable().setFocus();
+		editor.setFocus();
 	}
 
-	@Override
-	public IRezeptMetaInformation getMetaInformation() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 }
